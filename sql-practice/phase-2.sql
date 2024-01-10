67244fe559fece7c6ee149e1070af3d0120a8746
Joshua Hardin <joshhardinmusic@gmail.com>
2024-01-10 17:00:20 -0600

    ad

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,18 @@
 -- Your code here 
+DROP TABLE customers;
+DROP TABLE coffee_orders;
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(40),
+    phone INTEGER(10) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    points INTEGER DEFAULT 5 NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE coffee_orders (
+    id INTEGER PRIMARY KEY,
+    is_redeemed BOOLEAN,
+    ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
